621859304a14a3b5f585f08c8a63842d67b9a79c
Avelgest <avelgest@outlook.com>
2024-06-13 00:50:16 +0100

    Draw a checkered background for  images with alpha

--- widgets/picture_widget.cpp
+++ widgets/picture_widget.cpp
@@ -1,7 +1,10 @@
 #include "picture_widget.h"
 
+#include <array>
+
 #include <QtCore/Qt>
 
+#include <QtGui/QBitmap>
 #include <QtGui/QPaintEvent>
 #include <QtGui/QPainter>
 #include <QtWidgets/QMenu>
@@ -19,6 +22,33 @@ namespace
 {
     const QSize defaultSizeHint = {256, 256};
     const qreal minOpacity = 0.1;
+
+    // Draw a checker board pattern (for when drawing images with an alpha channel)
+    void drawCheckerBoard(QPainter &painter, const QRect &rect)
+    {
+        const int squareSize = 24;
+        const QColor color1("gray");
+        const QColor color2("darkgray");
+
+        // Create a 2*2 QBitmap texture with alternating values then scale it to squareSize
+        constexpr std::array<const uchar, 3> texBits = {1, 2, 0}; // bitmap rows (in binary): 01 10
+        static const QTransform texTransform = QTransform::fromScale(squareSize / 2., squareSize / 2.);
+        static const QBitmap tex = QBitmap::fromData({2, 2}, texBits.data()).transformed(texTransform);
+
+        QBrush brush;
+        brush.setColor(color2);
+        brush.setTexture(tex);
+
+        QPen pen;
+        pen.setWidth(0);
+
+        painter.setPen(pen);
+        painter.setRenderHint(QPainter::Antialiasing, false);
+
+        painter.fillRect(rect, color1);
+        painter.fillRect(rect, brush);
+    }
+
 } // namespace
 
 PictureWidget::PictureWidget()
@@ -87,6 +117,17 @@ void PictureWidget::paintEvent(QPaintEvent *event)
     ReferenceImage &refImage = *m_imageSP;
     const QImage &dispImage = refImage.displayImage();
 
+    // Draw a checkered background for images with alpha
+    if (refImage.baseImage().hasAlphaChannel() && referenceWindow()->windowMode() != GhostMode)
+    {
+        if (referenceWindow())
+        {
+            // Draw relative to the reference window's parent (looks better when cropping)
+            painter.setBrushOrigin(-referenceWindow()->pos());
+        }
+        drawCheckerBoard(painter, event->rect());
+    }
+
     // dispImage should be the same size as this->rect()
     if (destRect.size() != dispImage.size())
     {
@@ -154,6 +195,7 @@ void PictureWidget::setReferenceWindow(ReferenceWindow *refWindow)
     }
     m_referenceWindow = refWindow;
     QObject::connect(m_referenceWindow, &ReferenceWindow::activeImageChanged, this, &PictureWidget::setImage);
+    QObject::connect(m_referenceWindow, &ReferenceWindow::windowModeChanged, this, [this]() { update(); });
 }
 
 WindowMode PictureWidget::windowMode() const
